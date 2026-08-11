99ac893594a79914d934b2de68f21f1c9932ece6
mirella90567 <veiga.mirella@escola.pr.gov.br>
2026-08-11 12:30:13 -0300

    11.08

---
+++ script.js
@@ -0,0 +1,11 @@
+button{
+padding: 10px 20px;
+font-size: 18px;
+cursor: pointer;
+}
+const botao = document.getElementByld("tema");
+
+
+botao.addEventListener("click", function () {
+     document.body.classList.toggle("dark");
+});
